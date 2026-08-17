cc3d7c6b5f9ba9b881674b1f176b8584e705bebd
JustinMig <Justin@mayerig.com>
2026-08-17 16:04:31 -0500

    Add Messages Center page

---
+++ app/(crm)/messages/page.tsx
@@ -0,0 +1,8 @@
+import MessagesCenter from './MessagesCenter'
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
+export default function MessagesPage() {
+  return <MessagesCenter />
+}
